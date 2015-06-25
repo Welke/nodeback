54a447fae43a29ab42b98b6c25f1f1ff9bf9bd97
Cengizism <culusoy@ask-cs.com>
2015-06-25 15:06:13 +0200

    blog route adjusted for blog listing and post grabbing

--- routes/blog.js
+++ routes/blog.js
@@ -2,12 +2,21 @@ var modals = require('./modals');
 var express = require('express');
 var router = express.Router();
 
-router.get('/:category?', function (req, res) {
+router.get('/:section/:id?', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  var posts = modals.blog(req.params.category, 10);
+  var ret;
 
-  res.json(posts);
+  switch (req.params.section) {
+    case 'posts':
+      ret = modals.blog(req.params.section, 10);
+      break;
+    case 'post':
+      ret = modals.post();
+      break;
+  }
+
+  res.json(ret);
 });
 
 module.exports = router;
