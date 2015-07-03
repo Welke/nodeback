70d6aa58d56e4de491796cbf69e66724ff860981
Cengizism <culusoy@ask-cs.com>
2015-07-03 11:16:57 +0200

    updated locales content

--- routes/locales.js
+++ routes/locales.js
@@ -6,14 +6,10 @@ router.get('/', function (req, res) {
 
   var locales = {
     nl: {
-      HEAD_TITLE: 'Welke.nl | Elke dag een ontdekking',
-      HEAD_DESCRIPTION: 'Op zoek naar inspiratie? Je vindt hier vele foto\'s voor je interieur, lekkere recepten en leuke tips en ideeën. Je favoriete foto\'s sla je eenvoudig op in je eigen lookbook.',
-      HEAD_KEYWORDS: 'inspiratie, voorbeelden, ideeën, interieur, wonen, koken, recepten, gerechten, fashion, mode, beauty, wellness, tuin, terras, zelf maken, do-it-yourself, diy, hobby, kinderen'
+      TEST: 'NL TEST'
     },
     de: {
-      HEAD_TITLE: 'Spaaz.de | Entdecke. Sammel. Teile.',
-      HEAD_DESCRIPTION: 'Suchst Du Inspiration? Du kannst hier viele Fotos für jedes Zimmer des Hauses finden. Ideen für tolle Gerichte. Schlaue do-it-yourself Tipps. Bastelideen, Beauty- und Mode Tricks.  Deine Lieblings-Ideen kannst Du ganz einfach in Deinem eigenen Fotoalbum speichern.',
-      HEAD_KEYWORDS: 'Inspiration, Beispiele, Ideen, Einrichtung, wohnen, kochen, Rezepte, Gerichte, Fashion, Mode, Beauty, Wellness, Garten, Terrasse, basteln, selbermachen, do-it-yourself, diy, Kinder'
+      TEST: 'DE TEST'
     }
   };
 
